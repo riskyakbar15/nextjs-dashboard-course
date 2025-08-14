7a525746a569b30805cb2aca377b7d53bfd34a2c
riskyakbar15 <105841118223@student.ac.id>
2025-08-14 14:40:35 +0800

    update project files

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 import AcmeLogo from "@/app/ui/acme-logo";
 import { ArrowRightIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
-import styles from "@/app/ui/home.module.css";
 import { lusitana } from "@/app/ui/fonts";
 import Image from "next/image";
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+  title: 'Welcome to Acme',
+};
 
 export default function Page() {
   return (
